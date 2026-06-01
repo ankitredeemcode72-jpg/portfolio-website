c78ad6698aaf128f70046a5c49af0932f9aa7250
Ankit <ankitredeemcode72@gmail.com>
2026-06-02 00:08:28 +0530

    Day 23 dark mode added

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ alert("Welcome to My Portfolio Website!");
 
 document.getElementById("date").innerHTML =
 "Today's Date: " + new Date().toLocaleDateString();
+function darkMode() {
+    document.body.classList.toggle("dark");
+}
